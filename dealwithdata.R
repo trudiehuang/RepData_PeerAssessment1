8890a17bf8b3d18d8257bc6945d20b963648569d
trudiehuang <hqy19911@hotmail.com>
2016-01-10 14:16:56 +0800

    assignment

--- dealwithdata.R
+++ dealwithdata.R
@@ -1,86 +1,48 @@
----
-title: "PA1_template"
-author: "trudie"
-date: "2016年1月10日"
-output: pdf_document
----
-This is a report about the assignment 
-
-1. read the dataset and processing the data
-```{r}
+opts_chunk$set(echo = TRUE, fig.height = 4)
+#1Loading and preprocessing the data
 activity<- read.csv("activity.csv",header = TRUE)
 activity$date<-as.Date(activity$date)
+par(mfrow=c(1,1))
+
+#2What is mean total number of steps taken per day?
 actdate<-split(activity,activity$date)
 actdatemean<-sapply(actdate, function(x) sum(x$steps,na.rm = TRUE))
-```
-
-2.and the Histogram of the total number of steps taken each day 
-
-```{r}
-hist(actdatemean,main = "total steps taken each day",xlab = "steps each day")
-dev.copy(png,"instructions_fig/total_steps_taken_each_day.png")
-dev.off()
-```
-
-3.the mean and median number of steps taken each day
-
-```{r}
 summary(actdatemean)
-```
+hist(actdatemean,main = "total steps taken each day",xlab = "steps each day")
 
-processing the data
-```{r}
+#3What is the average daily activity pattern?
 actint<-split(activity,activity$interval)
 actintmean<-sapply(actint, function(x) mean(x$steps,na.rm = TRUE))
 interval<-names(actint)
-```
-4.Time series plot of the average number of steps taken
-```{r}
-plot(actintmean~interval,type="l",main="average daily activity pattern",ylab ="average number of steps taken" )
-dev.copy(png,"instructions_fig/average_daily_activity_pattern.png")
-dev.off()
-```
-
-5.The 5-minute interval that, on average, contains the maximum number of steps
-```{r}
+plot(actintmean~interval,type="l",main="average daily activity pattern")
 maxsteps<-interval[which.max(actintmean)]
-paste("the interval contains the maxinmum number of steps is",maxsteps,"-",as.numeric(maxsteps)+5)
-```
+maxsteps
 
-6. Describe and show a strategy for imputing missing data
-```{r}
+#4Imputing missing values
 #getting the total number of  missing value
 missvalue<- sum(is.na(activity$steps))
-paste("the number of missing value is",missvalue)
 
-##fill the missing data
+#fill the missing data
 activity1<-activity
-#figure out where is the na,turns out ,there are somedays have no data at all
-#so I just filling the missing value with the mean fo the 5-minute interval
+#figure out where is the na,turns out ,there are somedays have no data all
+#so I just filling the missing value with the mean fo that 5-minute interval
 nas<-sapply(actdate,function(x) sum(is.na(x$steps)))
 for (i in 1:17568) {
     if (is.na(activity$steps[i])){
     activity1$steps[i]<- actintmean[which(as.integer(interval)==activity$interval[i])]
     }
 }
-```
-
-7.Histogram of the total number of steps taken each day after missing values are imputed,and compare to the
-histogram before,it differ from the estimates from the first part of the assignment.
-After that ,the number of midlevel value grow up
-```{r}
 actdate1<-split(activity1,activity1$date)
 actdatemean1<-sapply(actdate1, function(x) sum(x$steps,na.rm = TRUE))
 summary(actdatemean1)
 par(mfrow=c(2,1))
 hist(actdatemean,main = "total steps taken each day",xlab = "steps each day")
 hist(actdatemean1,main = "total steps taken each day without missingvalue",xlab = "steps each day")
-dev.copy(png,"instructions_fig/Compare_total_steps_each_day.png")
-dev.off()
-```
 
-8.Panel plot comparing the average number of steps taken per 5-minute interval across weekdays and weekends
-```{r}
+#it differ from the estimates from the first part of the assignment.After that ,
+##the midlevel value grow up
+
+#5Are there differences in activity patterns between weekdays and weekends?
 par(mfrow=c(2,1))
 activity1$weekday<- weekdays(activity1$date)
 activity1$weekday<- with(activity1, sapply(weekday,function(x) if (sum(x== "星期六",x== "星期日")==1) {x <- "weekend"} else {x<- "weekday"}))
@@ -93,6 +55,3 @@ plot(actweekdaymean~intervalweekday,type="l",main="average daily activity patter
 actweekendmean<-sapply(actweekend, function(x) mean(x$steps,na.rm = TRUE))
 intervalweekend<-names(actweekend)
 plot(actweekendmean~intervalweekend,type="l",main="average daily activity pattern weekend",xlab = "")
-dev.copy(png,"instructions_fig/compare_daily_activity_pattern_weekday&weekend.png")
-dev.off()
-```
